567bcd13cc2b19e34011727ade8dcc0e0f31f741
ummekashaf <ummekashafhaifa@gmail.com>
2019-05-09 19:34:14 +0530

    object.js

---
+++ object.js
@@ -0,0 +1,14 @@
+var bookmyshow={
+    name:"MAHARSHI",
+    rating:4.0,
+    reviews:574,
+    comments:55,
+    time:10.30,
+    data:"10/2/19",
+    MovieType:["Drama","Action","Romance"]
+   
+}
+
+console.log(bookmyshow);
+
+console.log(bookmyshow.reviews);
